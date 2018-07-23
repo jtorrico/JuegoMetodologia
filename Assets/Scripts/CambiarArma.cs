839c64f281dbb50ca1e58cdd892316142f59d01d
Jonathan Torrico <jetorrico@gmail.com>
2018-07-23 18:21:37 -0300

    scripts #3
    agregado uso de bloqueo de caminos, funcionalidad de camara, cambio de arma, contador de enemigos, eliminacion de proyectiles y la barra de vida para jefes del juego

---
+++ Assets/Scripts/CambiarArma.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiarArma : MonoBehaviour {
+
+    // Objetos que representan a cada arma, como avatares.
+    public GameObject armaPistola, armaRifle, armaEscopeta;
+
+	// Use this for initialization
+	void Start () {
+
+		//En cada inicializacion siempre se activa el primer objeto (Pistola).
+		armaPistola.gameObject.SetActive (true);
+		armaRifle.gameObject.SetActive (false);
+        armaEscopeta.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        //Cuando se cambia de arma, se activa el objeto correspondiente y se desactivan los demas.
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            armaPistola.gameObject.SetActive(true);
+            armaRifle.gameObject.SetActive(false);
+            armaEscopeta.gameObject.SetActive(false);
+        }
+
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            armaPistola.gameObject.SetActive(false);
+            armaRifle.gameObject.SetActive(true);
+            armaEscopeta.gameObject.SetActive(false);
+        }
+
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            armaPistola.gameObject.SetActive(false);
+            armaRifle.gameObject.SetActive(false);
+            armaEscopeta.gameObject.SetActive(true);
+        }
+    }
+}
