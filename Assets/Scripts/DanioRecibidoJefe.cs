1f330e2534e977e394d727148dae5c7552088f99
Jonathan Torrico <jetorrico@gmail.com>
2018-07-24 22:53:22 -0300

    scripts #4
    agregado control de daño para el jugador, enemigos y jefes del juego junto con el controlador para pantalla de carga en seleccion de niveles.

---
+++ Assets/Scripts/DanioRecibidoJefe.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DanioRecibidoJefe : MonoBehaviour {
+
+    //Cantidad de vida del enemigo.
+    public float vidaActual = 1500;
+    private float vida;
+    public GameObject HPB;
+    public Image barraVida;
+    public Text cantidadVida;
+
+    void Start()
+    {
+        //Cada vez que se ejecuta el script, se le asigna el valor a la variable.
+        vida = vidaActual;
+    }
+    
+    void Update()
+    {
+
+        if (vida <= 0)
+        {
+            Destruir();
+        }
+    }
+
+    void Destruir()
+    {
+        //Si llega a 0HP, se destruye el objeto de la escena.
+        Destroy(gameObject);
+        DestroyObject(HPB);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        //Dependiendo del tipo de collision que reciba, diferenciadose a traves de tags, este disminuira la cantidad de vida con ciertos valores fijados.
+        Debug.Log("Colision");
+    
+        if (collision.gameObject.tag == "JugadorPistola")
+        {
+            vida -= 12;
+            cantidadVida.text = vida.ToString();
+            barraVida.fillAmount = vida / vidaActual;
+        }
+
+        if (collision.gameObject.tag == "JugadorRifle")
+        {
+            vida -= 8;
+            cantidadVida.text = vida.ToString();
+            barraVida.fillAmount = vida / vidaActual;
+        }
+
+        if (collision.gameObject.tag == "JugadorEscopeta")
+        {
+            vida -= 5;
+            cantidadVida.text = vida.ToString();
+            barraVida.fillAmount = vida / vidaActual;
+        }
+    }
+}
---
+++ Assets/Scripts/DanioRecibidoJugador.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DanioRecibidoJugador : MonoBehaviour
+{
+    //Cantidad de vida del jugador.
+    public int vida = 100;
+
+    //Uso de variables Serialize para el display de UI.
+    [SerializeField]Text displayVida;
+    [SerializeField]Text textoMuerte;
+
+    void Start()
+    {
+        //Cuando se inicia el script se muestra la cantidad de vida.
+        SetTextoVida();
+    }
+    
+    void Update()
+    {
+        if (vida <= 0)
+        {
+            
+            Muere();
+        }
+    }
+
+    //Si el jugador llega a 0HP, se destruye el objeto Jugador y reproduce el sonido de muerte, a la vez que tambien activa la pantalla de continuar.
+    void Muere()
+    {
+        Destroy(gameObject);
+        displayVida.text = "0";
+        textoMuerte.gameObject.SetActive(true);
+        FindObjectOfType<AudioManager>().Play("Muerte");
+    }
+
+    //Conversion a String de la cantidad de HP para la UI.
+    void SetTextoVida()
+    {
+
+        displayVida.text = vida.ToString();
+    }
+
+    //Se controla la disminucion de HP a traves de colisiones, cuando el enemigo toca al jugador este pierde vida, cuando toca un MedKit recupera vida, tambien se incluye la funcion para
+    //actualizar la cantidad de HP actual y la reproduccion de los sonidos asignados.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Enemigo")
+        {
+            Debug.Log("Colision");
+            vida -= 12;
+            SetTextoVida();
+            FindObjectOfType<AudioManager>().Play("Golpe");
+        }
+
+        if (collision.gameObject.tag == "PaqueteVida")
+        {
+            Debug.Log("Colision Paquete Vida!");
+            vida += 25;
+            SetTextoVida();
+            FindObjectOfType<AudioManager>().Play("Curacion");
+        }
+    }
+}
