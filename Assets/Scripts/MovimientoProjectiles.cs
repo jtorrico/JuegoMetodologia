31e2a834c3e86154ce30432c7376d815a750b5d8
Jonathan Torrico <jetorrico@gmail.com>
2018-07-28 20:08:07 -0300

    scripts #7
    agregado funcionalidad de trayectoria de projectiles, activacion del paquete de vida y los controles para el movimiento del jugador.

---
+++ Assets/Scripts/MovimientoProjectiles.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoProjectiles : MonoBehaviour {
+
+    //Velocidad maxima del projectil.
+    float velocidadMaxima = 40;
+
+	//Metodo utilizado para el movimiento de los projectiles de las armas.
+	void Update () {
+
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(0, velocidadMaxima * Time.deltaTime, 0);
+        pos += transform.rotation * velocity;
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/PaqueteVida.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaqueteVida : MonoBehaviour {
+
+    //Cuando colisiona con el Jugador, el objeto se destruye.
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Jugador")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
