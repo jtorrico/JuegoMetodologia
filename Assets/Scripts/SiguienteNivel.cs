b0f96faa24e332df1b8e616ba999688123ea086b
Jonathan Torrico <jetorrico@gmail.com>
2018-07-30 19:21:17 -0300

    scripts #9
    agregado función de control de volumen y la operación de carga de siguiente nivel con un mensaje indicador

---
+++ Assets/Scripts/SiguienteNivel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SiguienteNivel : MonoBehaviour {
+
+    //Mensaje que muestra que se esta cargando el siguiente nivel.
+    public GameObject mensaje;
+
+    //Metodo que detecta la colision del jugador en un punto marcado en el nivel actual, al activarse, muestra la advertencia y carga el siguiente nivel.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Jugador")
+        {
+            mensaje.SetActive(true);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
